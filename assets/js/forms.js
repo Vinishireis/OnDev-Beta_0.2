fbc013f5a1b77b1a8d4fe76c53d13dcf0dd952d0
Vinishireis <nishimuravinicius28@gmail.com>
2024-05-04 16:21:03 -0300

    Update Code OnDev
    Co-Authored-By: YoSoyTeodoro <145409630+YoSoyTeodoro@users.noreply.github.com>

--- assets/js/forms.js
+++ assets/js/forms.js
@@ -221,3 +221,22 @@ window.addEventListener('resize', function() {
 	  signinSignupColaborator.style.overflowX = 'hidden';
 	}
   });
+
+  
+const form = document.querySelector("form"),
+nextBtn = form.querySelector(".nextBtn"),
+backBtn = form.querySelector(".backBtn"),
+allInput = form.querySelectorAll(".first input");
+
+
+nextBtn.addEventListener("click", ()=> {
+allInput.forEach(input => {
+if(input.value != ""){
+	form.classList.add('secActive');
+}else{
+	form.classList.remove('secActive');
+}
+})
+})
+
+backBtn.addEventListener("click", () => form.classList.remove('secActive'));
